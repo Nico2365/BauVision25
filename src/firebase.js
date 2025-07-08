8772dd90eabe2d89d01736fc74ea27b00019a3e8
Nico2365 <nico.gruendler@gmx.de>
2025-07-08 13:32:52 +0200

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCS7Q0h6-Pc-kB1Dqa-WlB2LdaLnl4zaY4",
@@ -17,4 +16,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
